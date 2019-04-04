f49d5e710d5f48c8d6d07cc949aee793aa94a6d7
Suyash Ganu <ssganu8@gmail.com>
2019-04-04 16:45:45 -0700

    logging our side's Pokemon and logging moves taken

--- ps_main.py
+++ ps_main.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 from random import randint
-
+from selenium.webdriver.common.action_chains import ActionChains
 
 class PokemonBot():
 	def __init__(self):
@@ -13,19 +13,38 @@ class PokemonBot():
 		button = self.browser.find_element_by_css_selector('.button.mainmenu1.big')
 		button.click()
 		time.sleep(2)
-		self.browser.find_element_by_name('username').send_keys("ssganu13")
+		self.browser.find_element_by_name('username').send_keys("zizisquid123")
 		time.sleep(1)
 		self.browser.find_element_by_xpath("/html/body/div[4]/div/form/p[2]/button[1]").click()
 		time.sleep(1)
 		self.browser.find_element_by_css_selector('.button.mainmenu1.big').click()
 
-		time.sleep(8)
+		time.sleep(10)
+
+		# Loads up data for our side's Pokemon
+		for x in range(5):
+			switch = self.browser.find_elements_by_name('chooseSwitch')[x]
+			ActionChains(self.browser).move_to_element(switch).perform()
+			time.sleep(1) 
+			print(self.browser.find_element_by_xpath("//*[@id=\"tooltipwrapper\"]/div/div/h2").text)
+			print(self.browser.find_element_by_xpath("//*[@id=\"tooltipwrapper\"]/div/div/p[1]").text)
+			print(self.browser.find_element_by_xpath("//*[@id=\"tooltipwrapper\"]/div/div/p[2]").text)
+			print(self.browser.find_element_by_xpath("//*[@id=\"tooltipwrapper\"]/div/div/p[3]").text)
+			print(self.browser.find_element_by_xpath("//*[@id=\"tooltipwrapper\"]/div/div/p[4]").text)
 		
-		
+		# Currently just makes random moves and logs it.
 		while(True):
 			try: 
 				random = randint(0, 3)
-				self.browser.find_elements_by_name('chooseMove')[random].click()
+				# print(self.browser.find_elements_by_name('choosename')[random].getText())
+				move = self.browser.find_elements_by_name('chooseMove')[random]
+
+				
+
+
+				print(move.get_attribute('data-move'))
+				print(move.get_attribute('class'))
+				move.click()
 			except:
 				x = 1
 
