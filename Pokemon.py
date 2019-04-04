e709d51fe6adcf168a98ba617b0453c0aad23ae3
ZackFiner <zackary.finer@yahoo.com>
2019-04-03 17:52:05 -0700

    added damage calulation

--- Pokemon.py
+++ Pokemon.py
@@ -1,5 +1,6 @@
 import requests
 import json
+GLOBAL_MOD = 1.0
 def getPokemonFromAPI(ID:int) -> dict:
     url = "https://pokeapi.co/api/v2/pokemon/"+str(ID)+"/"
     url_d = requests.get(url)
@@ -7,6 +8,8 @@ def getPokemonFromAPI(ID:int) -> dict:
 class Pokemon:
     def __init__(self, ID):
         self.data_dict = getPokemonFromAPI(ID)
+    def Damage(self, other, move):
+        dmg = ( ( (2*other.lvl/5 + 2)*move.power*self.stat["Attack"]/other.stat["Defense"] )/50 + 2 ) * GLOBAL_MOD
 
 a = Pokemon(1)
 print("done.")
