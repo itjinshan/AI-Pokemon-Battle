e8cb74968c139c1007e8fad9039c9ec408f167a8
ZackFiner <zackary.finer@yahoo.com>
2019-05-03 12:48:10 -0700

    I've updated this class to pull the following information from the API: priority, power points (pp), power, type, priority, accuracy
    Moves can be pulled from the API via their Name or a URL.
    
    keep in mind that not all of these moves have the same parameters, so
    they ma come back with null attributes.

--- Move.py
+++ Move.py
@@ -13,8 +13,18 @@ def getMoveFromAPI(ID:int=None, Name:str=None, URL:str=None) -> dict:
     return url_d.json()
 
 class Move:
-    def __init__(self, URL):
-        self.data_dict = getMoveFromAPI(URL=URL)
+    def __init__(self, URL:str = None, Name:str = None):
+        self.data_dict = getMoveFromAPI(Name=Name, URL=URL)
         self.power = self.data_dict["power"]
         self.accuracy = self.data_dict["accuracy"]
         self.power_points = self.data_dict["pp"]
+        self.type = self.data_dict["type"]["name"]
+        self.priority = self.data_dict["priority"]
+
+d = Move(Name="flamethrower")
+print(d.accuracy)
+print(d.power)
+print(d.power_points)
+print(d.type)
+print(d.priority)
+
