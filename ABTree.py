51097f216db19a550a35e527183af28d22a9cba6
Alex Frank <alexfrankcodes@gmail.com>
2019-05-18 21:31:04 -0700

    Removed unnecessary ABTree file

--- ABTree.py
+++
@@ -1,8 +0,0 @@
-# This algorithm will use the information from our knowledge base about the current play session
-# to asses which moves are most effective. The current evaluation function will be something simple:
-# Our HP/their HP
-
-def assessUtility(defender, aggressor):
-
-
-def minmax(node, depth, isMax, alpha, beta):
